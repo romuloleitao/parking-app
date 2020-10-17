c59aaed0bc3525e707c089ccecd2cbbd4a31944c
romuloleitao <romulojleitao@gmail.com>
2020-10-17 00:43:13 -0400

    Adding required to model properties.

--- Models/Car.cs
+++ Models/Car.cs
@@ -1,13 +1,22 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Parking.Models
 {
     public class Car
     {
         public int Id { get; set; }
+
+        [Required]
         public string TypeName { get; set; }
+
+        [Required]
         public string LicensePlate { get; set; }
+
+        [Required]
         public string EntryTime { get; set; }
+
+        [Required]
         public string DepartureTime { get; set; }
     }
 }
