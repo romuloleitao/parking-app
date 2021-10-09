835b21e440bb172b2dcd4fc4b9318b8cb35c5d4a
romuloleitao <romulojleitao@gmail.com>
2021-10-08 23:40:06 -0400

    Adding components

--- client-parking/src/App.js
+++ client-parking/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Home } from './components/Home';
+import { AddCar } from './components/AddCar';
+import { EditCar } from './components/EditCar';
 
 function App() {
   return (
     <div className="App">
-
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/add" component={AddCar} />
+        <Route path="/edit/:id" component={EditCar} />
+      </Switch>
+    </Router>
     </div>
   );
 }
